2512628f6dd00d77098082acde8c036414086809
Zanin001 <gbrl.zanin99@gmail.com>
2023-03-04 18:37:36 -0300

    Created Blazor Server and started front-end

---
+++ NetworkService/NetworkService/Shared/MainLayout.razor.designer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+using Radzen.Blazor;
+
+namespace NetworkService.Shared;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    protected DialogService DialogService { get; set; }
+
+    [Inject]
+    protected TooltipService TooltipService { get; set; }
+
+    [Inject]
+    protected ContextMenuService ContextMenuService { get; set; }
+
+    [Inject]
+    protected NotificationService NotificationService { get; set; }
+
+    protected RadzenBody MainBody;
+    protected RadzenSidebar MainSidebar;
+
+    protected async Task MainSidebarToggleClick(dynamic args)
+    {
+        await InvokeAsync(() => { MainBody.Toggle(); });
+
+        await InvokeAsync(() => { MainSidebar.Toggle(); });
+    }
+}
