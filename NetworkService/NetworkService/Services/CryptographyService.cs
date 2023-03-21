37524fbb09cc5211782976425ac2771ee2c0ce90
Gabriel Zanin <gabriel.zanin@solidinvent.com.br>
2023-03-21 20:49:57 -0300

    Started Cryptography service and Smtp Service

---
+++ NetworkService/NetworkService/Services/CryptographyService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Security.Cryptography;
+
+namespace NetworkService.Services
+{
+    public class CryptographyService
+    {
+        private readonly string Key = "ntwService";
+        
+
+        public async Task<string> Encrypt(string cleanText)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ NetworkService/NetworkService/Services/SmtpService.cs
@@ -0,0 +1,20 @@
+﻿using NetworkService.Models;
+using System.Net.Mail;
+
+namespace NetworkService.Services
+{
+    public class SmtpService
+    {
+        public async Task SendEmail(SmtpSettings smtpSettings)
+        {
+            try
+            {
+                //SmtpClient
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
