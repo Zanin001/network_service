4f8119aa60b06595e074e3ff2d2dc669bbb959cb
Zanin001 <gbrl.zanin99@gmail.com>
2023-03-21 21:22:20 -0300

    Created QueryService

---
+++ NetworkService/NetworkService/Data/QueryService.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq.Expressions;
+
+namespace NetworkService.Data
+{
+    public class QueryService
+    {
+        private readonly NetworkServiceDbContext _context;
+
+        public QueryService(NetworkServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CreateItem<TData>(TData item) where TData : class
+        {
+            int result = default;
+
+            try
+            {
+                await _context.AddAsync(item);
+                result = await _context.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return result;
+        }
+        public async Task<TData> ReadItem<TData>(Expression<Func<TData, bool>> where) where TData : class
+        {
+            var dbSet = _context.Set<TData>();
+
+            return dbSet.FirstOrDefault(where);
+        }
+        public async Task<TData[]> ReadList<TData>(Expression<Func<TData, bool>> where) where TData : class
+        {
+            var dbSet =_context.Set<TData>();
+
+            return dbSet.Where(where).ToArray();
+        }
+        public async Task<int> UpdateItem<TData>(TData item) where TData : class
+        {
+            int result = default;
+
+            try
+            {
+                _context.Update(item);
+                result = await _context.SaveChangesAsync();
+            }
+            catch(Exception ex) { Console.WriteLine(); }
+
+            return result;
+        }
+        public async Task<int> DeleteItem<TData>(TData item) where TData : class
+        {
+            int result = default;
+
+            try
+            {
+                _context.Remove(item);
+                result = await _context.SaveChangesAsync();
+            }
+            catch(Exception ex) { Console.WriteLine(); }
+
+            return result;
+        }
+
+
+    }
+}
--- NetworkService/NetworkService/Services/CryptographyService.cs
+++ NetworkService/NetworkService/Services/CryptographyService.cs
@@ -18,6 +18,8 @@ namespace NetworkService.Services
             {
 
             }
+
+            return default;
         }
     }
 }
