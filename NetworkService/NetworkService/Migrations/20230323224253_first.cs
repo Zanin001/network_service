e6fa334e38ffc78d97bf372c5a21a435e4a6b796
Gabriel Zanin <gabriel.zanin@solidinvent.com.br>
2023-03-23 20:05:48 -0300

    Finished smtp settings create

---
+++ NetworkService/NetworkService/Migrations/20230323224253_first.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkService.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "SmtpSettings",
+                columns: table => new
+                {
+                    SmtpSettingsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SMTP = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Port = table.Column<int>(type: "int", nullable: false),
+                    SSL = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SmtpSettings", x => x.SmtpSettingsId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SmtpSettings");
+        }
+    }
+}
---
+++ NetworkService/NetworkService/Models/DatabaseConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace NetworkService.Models
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; set; }
+        public string DBVersion { get; set; }
+    }
+}
