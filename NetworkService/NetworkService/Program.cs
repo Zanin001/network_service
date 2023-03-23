8d04224a39f9309081e147236494443ece5344b1
Zanin001 <gbrl.zanin99@gmail.com>
2023-03-23 20:10:21 -0300

    Revert "Revert "Finished smtp settings create""
    This reverts commit 14f5a3a794d93a911a11a74256eba8581bef9168.

--- NetworkService/NetworkService/Program.cs
+++ NetworkService/NetworkService/Program.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using NetworkService.Data;
 using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var DatabaseConfig = builder.Configuration.GetSection(nameof(NetworkService.Models.DatabaseConfig)).Get<NetworkService.Models.DatabaseConfig>();
+
+builder.Services.AddDbContext<NetworkServiceDbContext>(option => option.UseMySql(
+    DatabaseConfig.ConnectionString,
+    ServerVersion.Parse(DatabaseConfig.DBVersion)
+));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -12,8 +20,7 @@ builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<ContextMenuService>();
 builder.Services.AddScoped<TooltipService>();
-
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<QueryService>();
 
 var app = builder.Build();
 
