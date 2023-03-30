598bda89bcba529f8070bf50d5fb88926117629d
Zanin001 <gbrl.zanin99@gmail.com>
2023-03-30 20:27:19 -0300

    Started send email page

---
+++ NetworkService/NetworkService/Models/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace NetworkService.Models
+{
+    public class Email
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Recipient { get; set; }
+    }
+}
