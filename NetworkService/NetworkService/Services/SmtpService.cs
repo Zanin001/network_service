6620e4dc2a368752f022e8db8f3d4af56f59d5da
Zanin001 <gbrl.zanin99@gmail.com>
2023-04-03 20:33:39 -0300

    Created smtp service and added to page

--- NetworkService/NetworkService/Services/SmtpService.cs
+++ NetworkService/NetworkService/Services/SmtpService.cs
@@ -1,20 +1,47 @@
 ﻿using NetworkService.Models;
+using System.Net;
 using System.Net.Mail;
 
 namespace NetworkService.Services
 {
     public class SmtpService
     {
-        public async Task SendEmail(SmtpSettings smtpSettings)
+        public static async Task<bool> SendEmail(SmtpSettings smtpSettings, Email email)
         {
+            bool response = default;
+
             try
             {
-                //SmtpClient
+                using var smtpClient = new SmtpClient(smtpSettings.SMTP)
+                {
+                    Port = smtpSettings.Port,
+                    Credentials = new NetworkCredential(smtpSettings.Email, smtpSettings.Password),
+                    EnableSsl = smtpSettings.SSL
+                };
+
+                using var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(smtpSettings.Email),
+                    Subject = email.Subject,
+                    Body = email.Body,
+                    IsBodyHtml = true
+                };
+
+                foreach (var mail in email.Recipients)
+                    mailMessage.To.Add(mail);
+
+                smtpClient.Send(mailMessage);
+
+                response = true;
+
             }
             catch (Exception ex)
             {
+                response = false;
                 Console.WriteLine(ex);
-            }
+            }   
+
+            return response;
         }
     }
 }
